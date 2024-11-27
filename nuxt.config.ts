b5170f7c2549fccd5499693aff9ba09cf5409729
Katunga <justus.mutua@spinmobile.co.ke>
2024-11-27 17:35:43 +0300

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,57 @@
 import Aura from "@primevue/themes/aura";
 import PrimeVue from "primevue/config";
 import { definePreset } from "@primevue/themes";
-
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "{green.50}",
+      100: "{green.100}",
+      200: "{green.200}",
+      300: "{green.300}",
+      400: "{green.500}",
+      500: "{green.600}",
+      600: "{green.700}",
+      700: "{green.700}",
+      800: "{green.800}",
+      900: "{green.900}",
+      950: "{green.950}",
+    },
+    colorScheme: {
+      light: {
+        surface: {
+          0: "#ffffff",
+          50: "{gray.50}",
+          100: "{gray.100}",
+          200: "{gray.200}",
+          300: "{gray.300}",
+          400: "{gray.400}",
+          500: "{gray.500}",
+          600: "{gray.600}",
+          700: "{gray.700}",
+          800: "{gray.700}",
+          900: "{gray.800}",
+          950: "{gray.800}",
+        },
+      },
+      dark: {
+        surface: {
+          0: "#ffffff",
+          50: "{gray.50}",
+          100: "{gray.100}",
+          200: "{gray.200}",
+          300: "{gray.300}",
+          400: "{gray.400}",
+          500: "{gray.500}",
+          600: "{gray.600}",
+          700: "{gray.700}",
+          800: "{gray.700}",
+          900: "{gray.800}",
+          950: "{gray.800}",
+        },
+      },
+    },
+  },
+});
 export default defineNuxtConfig({
   // app: {
   //   layoutTransition: { name: "layout", mode: "out-in" },
@@ -21,6 +71,7 @@ export default defineNuxtConfig({
     /* Configuration */
     options: {
       theme: {
+        preset: MyPreset,
         options: { darkModeSelector: ".dark" },
       },
     },
